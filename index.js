b5d2118862f52b771ee559f31fcff3a362bf40be
adrwtrm <151663868+adrwtrm@users.noreply.github.com>
2025-05-12 02:59:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
 const Eris = require("eris");
-const keep_alive = require('./keep_alive.js')
+const keep_alive = require('./keep_alive.js');
 
 // Replace TOKEN with your bot account's token
 const bot = new Eris(process.env.token);
 
-bot.on("error", (err) => {
-  console.error(err); // or your preferred logger
+bot.on("ready", () => {
+  console.log("Bot is ready!");
+  
+  // Set the status
+  bot.editStatus("online", {
+    name: "$5 UBEREATS IN BIO",
+    type: 0 // 0 = Playing
+  });
 });
 
-bot.connect(); // Get the bot to connect to Discord
+bot.on("error", (err) => {
+  console.error(err);
+});
+
+bot.connect();
